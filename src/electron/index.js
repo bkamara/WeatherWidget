9f46dec16292829e58452baf54e07d4fbae53aea
Bisola Kamara <bisola.kamara@neudesic.com>
2017-07-06 15:29:56 -0700

    Icon updates

--- src/electron/index.js
+++ src/electron/index.js
@@ -6,7 +6,7 @@ let win
  
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({icon: __dirname + '/icon.png',width: 1000, height: 650})
+  win = new BrowserWindow({icon: __dirname + '/assets/icon.ico',width: 1000, height: 650})
   
   win.setResizable(false);
   // and load the index.html of the app.
